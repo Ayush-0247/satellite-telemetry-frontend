78d9119180543121aad2eb894ea7c06e9cf9140c
Ayush <rishischnabel@gmail.com>
2026-07-02 00:28:37 +0530

    fix1

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,10 @@ import axios from "axios";
 // https://satellite-telemetry-project-auth.onrender.com
 const api = axios.create({
   // baseURL: "https://satellite-telemetry-project-auth.onrender.com/api",
-  baseURL: "https://satellite-telemetry-backend.onrender.com/api",
+  baseURL: "https://satellite-telemetry-backend-1.onrender.com/api",
+  // https://satellite-telemetry-backend-1.onrender.com/
 });
-
+  
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {

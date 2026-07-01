b9349f5d8abc1baa728facba22f4cbca836abe89
Ayush <rishischnabel@gmail.com>
2026-07-01 12:18:02 +0530

    Initial commit

---
+++ src/api/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+// https://satellite-telemetry-project-auth.onrender.com
+const api = axios.create({
+  // baseURL: "https://satellite-telemetry-project-auth.onrender.com/api",
+  baseURL: "https://satellite-telemetry-backend.onrender.com/api",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
